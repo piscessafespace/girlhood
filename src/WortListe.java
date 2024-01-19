84525b828b2224c0c42bea4060e0a76f8d92fe7b
piscessafespace <97235471+piscessafespace@users.noreply.github.com>
2024-01-19 21:06:51 +0100

    Auskommentieren von WortListe und KLEINE verfeinerungen am Code zum Verringern des Codes

--- src/WortListe.java
+++ src/WortListe.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+
 /**
  * Klasse für eine Wortliste Klasse
  * @author Aho 5bhit
@@ -6,80 +7,82 @@ import java.util.Arrays;
  */
 public class WortListe {
 
-    private WortEintrag[] eintrag;
+    private WortEintrag[] eintrag; // Array, das die WortEinträge speichert
 
+    // Konstruktor für eine leere Wortliste
     public WortListe() {
         this.eintrag = new WortEintrag[0];
-
     }
 
+    // Konstruktor für eine Wortliste mit einer maximalen Anzahl von Einträgen
     public WortListe(int maxw) {
         this.eintrag = new WortEintrag[maxw];
-
     }
 
+    // Methode zum Hinzufügen eines WortEintrags zur Liste
     public boolean addWort(WortEintrag www) {
         boolean added = false;
 
-        for (int i = 0; i < eintrag.length && added == false; i++) {
+        for (int i = 0; i < eintrag.length && !added; i++) {
             if (this.eintrag[i] == null) {
                 this.eintrag[i] = www;
                 added = true;
-
             }
         }
-        if (added == false) {
+
+        // Wenn kein Platz gefunden wurde, wird das Array um einen Platz erweitert
+        if (!added) {
             this.eintrag = Arrays.copyOf(this.eintrag, this.eintrag.length + 1);
             this.eintrag[this.eintrag.length - 1] = www;
-
         }
         return added;
-
     }
 
+    // Methode zum Abrufen eines WortEintrags an einer bestimmten Position im Array
     public WortEintrag getEintrag(int index) {
         for (int i = 0; i < eintrag.length; i++) {
             if (eintrag[i] != null) {
                 if (i == index) {
                     return this.eintrag[i];
-
                 }
             }
         }
+        // Wenn die angegebene Position nicht vorhanden ist, wird der erste Eintrag zurückgegeben
         return this.eintrag[0];
-
     }
 
+    // Methode zum Abrufen des gesamten Arrays von WortEinträgen
     public WortEintrag[] getEintragArray() {
         return this.eintrag;
-
     }
 
+    // Methode zur Rückgabe der Länge der Wortliste (Anzahl der Einträge)
     public int getLength(){
         return eintrag.length;
     }
 
+    // Überschriebene Methode für die Darstellung der Wortliste als String
     @Override
     public String toString(){
         String liste = "";
 
+        // Zusammenstellung der Wortliste als Zeichenkette
         for(int i = 0 ; i < getLength() ; i++){
             liste += "\n" + this.eintrag[i];
-
         }
         return liste;
-
     }
 
+    // Methode zum Löschen eines WortEintrags anhand des Wortes
     public boolean DelWort(String word){
         for(int i = 0 ; i < getLength() ; i++){
             if (eintrag[i] != null) {
                 if(eintrag[i].getWort().equals(word)) {
-                    eintrag[i] = null;
+                    eintrag[i] = null; // Löschen des Eintrags
                     return true;
                 }
             }
         }
-        return false;
+        return false; // Rückgabe false, wenn das Wort nicht gefunden wurde
     }
 }
