63f1649cf12a0043911aec53343d4733ad6f8299
piscessafespace <97235471+piscessafespace@users.noreply.github.com>
2024-01-19 20:21:09 +0100

    Interface Funktionen reingeben (toSting) und sie alle overriden in WortEintrag und WortListe. Die Funktionen fuer GK in Wort Eintrag und SULInterface implementiert

--- src/SuLInterface.java
+++ src/SuLInterface.java
@@ -1,2 +1,12 @@
+import java.io.IOException;
+
 public interface SuLInterface {
+
+    public interface SpeichernUndLadenInterface {
+        public void speichern(String filename) throws IOException;
+        public void laden(String filename) throws IOException;
+        public void speichern() throws IOException;
+        public void laden() throws IOException;
+
+    }
 }
--- src/WortListe.java
+++ src/WortListe.java
@@ -6,4 +6,80 @@ import java.util.Arrays;
  */
 public class WortListe {
 
+    private WortEintrag[] eintrag;
+
+    public WortListe() {
+        this.eintrag = new WortEintrag[0];
+
+    }
+
+    public WortListe(int maxw) {
+        this.eintrag = new WortEintrag[maxw];
+
+    }
+
+    public boolean addWort(WortEintrag www) {
+        boolean added = false;
+
+        for (int i = 0; i < eintrag.length && added == false; i++) {
+            if (this.eintrag[i] == null) {
+                this.eintrag[i] = www;
+                added = true;
+
+            }
+        }
+        if (added == false) {
+            this.eintrag = Arrays.copyOf(this.eintrag, this.eintrag.length + 1);
+            this.eintrag[this.eintrag.length - 1] = www;
+
+        }
+        return added;
+
+    }
+
+    public WortEintrag getEintrag(int index) {
+        for (int i = 0; i < eintrag.length; i++) {
+            if (eintrag[i] != null) {
+                if (i == index) {
+                    return this.eintrag[i];
+
+                }
+            }
+        }
+        return this.eintrag[0];
+
+    }
+
+    public WortEintrag[] getEintragArray() {
+        return this.eintrag;
+
+    }
+
+    public int getLength(){
+        return eintrag.length;
+    }
+
+    @Override
+    public String toString(){
+        String liste = "";
+
+        for(int i = 0 ; i < getLength() ; i++){
+            liste += "\n" + this.eintrag[i];
+
+        }
+        return liste;
+
+    }
+
+    public boolean DelWort(String word){
+        for(int i = 0 ; i < getLength() ; i++){
+            if (eintrag[i] != null) {
+                if(eintrag[i].getWort().equals(word)) {
+                    eintrag[i] = null;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
