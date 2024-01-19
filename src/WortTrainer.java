a5d849ffbe5e2bda324a1951ec1fdcc0fa4ca143
piscessafespace <97235471+piscessafespace@users.noreply.github.com>
2024-01-19 20:55:01 +0100

    Wort Trainer completed und die Teile der anderen Klassen eingefuegt. Cute meow meow

--- src/WortTrainer.java
+++ src/WortTrainer.java
@@ -4,16 +4,24 @@ import java.util.*;
  * Klasse für einen Worttrainer
  * @author Aho 5bhit
  * @version 19.01.2024
+ * If only              ╱|、
+ * meow               (˚ˎ。7
+ * meow                |、˜〵
+ * meow               じしˍ,)ノ
+ * &  meow        ▒▒▒▒▒▒
  */
 
 public class WortTrainer {
+
+    private WortListe l;
 
     private int questions;
     private int right;
     private int wrong;
 
-    public WortTrainer(){
+    public WortTrainer(WortListe liste){
 
+        this.l = liste;
         this.questions = 0;
         this.right= 0;
         this.wrong =0;
@@ -68,6 +76,60 @@ public class WortTrainer {
 
     public String getFehlerQuote(){
         return new String("Von meow " +questions+ " Fragen sind meow "+right+" meowichtig & "+wrong+" meowalsch");
+
+    }
+
+    public WortEintrag getZufallsWort() {
+        Random random = new Random(this.l.getLength());
+        int r = random.nextInt(this.l.getLength());
+
+        for (int i = 0; i < this.l.getLength(); i++) {
+            if(this.l.getEintrag(r)!=null){
+                return this.l.getEintrag(r);
+
+            }
+        }
+        return null;
+
+    }
+
+    //Das werden wir dann in der App Klasse verwenden
+    public WortEintrag getWort(int iii){
+        return this.l.getEintrag(iii);
+
+    }
+
+    //Das hier auch
+    public WortListe getWortListe(){
+        return this.l;
+
+    }
+
+    public boolean check(String w, int iii){
+        this.questions++;
+
+        if(this.l.getEintrag(iii).getWort().equals(w)) {
+            this.right++;
+            return true;
+
+        }
+        this.wrong++;
+        return false;
+
+    }
+
+
+    public boolean checkIgnoreCase(String w, int iii){
+        this.questions++;
+
+        //macht eif beide Woerter in uppercase und schaut nach ob sie immernoch dasselbe wort sind, wenn ja gibts noch einen Extra Punkt
+        if(this.l.getEintrag(iii).getWort().toUpperCase().equals(w.toUpperCase())) {
+            this.right++;
+            return true;
+
+        }
+        this.wrong++;
+        return false;
 
     }
 }
