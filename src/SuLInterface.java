b0c072e35f6f20538584b1ae0c85dfa44b1a0a6f
piscessafespace <97235471+piscessafespace@users.noreply.github.com>
2024-01-19 20:23:45 +0100

    Strategy Pattern Interface

--- src/SuLInterface.java
+++ src/SuLInterface.java
@@ -1,5 +1,10 @@
 import java.io.IOException;
 
+/**
+ * Das Interface fuer Strategy Pattern
+ * @author Aho 5bhit
+ * @version 19.01.2024
+ */
 public interface SuLInterface {
 
     public interface SpeichernUndLadenInterface {
