76e6fbc8f80bc234d347e11c3df56d1615d81034
piscessafespace <97235471+piscessafespace@users.noreply.github.com>
2024-01-19 23:15:58 +0100

    SpeichernLadenTxt auskommentiert. bin endlich FERTIG, ich mach noch ein LETZTEN Commit und gebe ueberall katzen rein >:)

--- src/SpeichernLadenTxt.java
+++ src/SpeichernLadenTxt.java
@@ -6,57 +6,82 @@ import java.util.Scanner;
  * @version 19.01.2024
  */
 public class SpeichernLadenTxt implements SuLInterface{
-    private WortTrainer trainer;
-    private String filename = "Worttrainer.txt";
+    // Klasse zum Speichern und Laden von WortTrainer-Daten in einer Textdatei
 
-    public SpeichernLadenTxt(WortTrainer t){
-        this.trainer = t;
-    }
+        // Instanzvariable für den WortTrainer
+        private WortTrainer trainer;
 
-    public WortEintrag getWort(int index) {
-        return this.trainer.getWort(index);
-    }
+        // Standarddateiname für das Speichern und Laden
+        private String filename = "Worttrainer.txt";
 
-    public int getTrue(){
-        return this.trainer.getRight();
-    }
-
-    public int getFalse(){
-        return this.trainer.getWrong();
-    }
-
-    public int getTries(){
-        return this.trainer.getQuestions();
-    }
-
-    public void speichern(String filename) throws IOException{
-        File f = new File(filename);
-        BufferedWriter writer = null;
-        writer = new BufferedWriter(new FileWriter(f));
-        writer.write(trainer.getQuestions() + System.lineSeparator() +trainer.getRight() + System.lineSeparator() +trainer.getWrong() + System.lineSeparator());
-        writer.close();
-    }
-
-    public void speichern() throws IOException{
-        speichern(this.filename);
-    }
-
-
-    public void laden(String filename) throws IOException {
-        Scanner reader = new Scanner(new BufferedReader(new FileReader(filename)));
-        try {
-            int fragen = Integer.parseInt(reader.nextLine());
-            int richtige = Integer.parseInt(reader.nextLine());
-            int falsche = Integer.parseInt(reader.nextLine());
-            trainer.addQuestions(fragen);
-            trainer.addRight(richtige);
-            trainer.addWrong(falsche);
-        } finally {
-            reader.close();
+        // Konstruktor, der den WortTrainer übernimmt
+        public SpeichernLadenTxt(WortTrainer t){
+            this.trainer = t;
         }
-    }
 
-    public void laden() throws IOException {
-        laden(this.filename);
-    }
+        // Methode, um ein WortEintrag-Objekt anhand des Index zu erhalten
+        public WortEintrag getWort(int index) {
+            return this.trainer.getWort(index);
+        }
+
+        // Methoden, um die Anzahl der richtigen, falschen und insgesamt gestellten Fragen abzurufen
+        public int getTrue(){
+            return this.trainer.getRight();
+        }
+
+        public int getFalse(){
+            return this.trainer.getWrong();
+        }
+
+        public int getTries(){
+            return this.trainer.getQuestions();
+        }
+
+        // Methode zum Speichern von Trainerdaten in einer Textdatei
+        public void speichern(String filename) throws IOException{
+            File f = new File(filename);
+            BufferedWriter writer = null;
+            try {
+                // FileWriter zum Schreiben in die Datei
+                writer = new BufferedWriter(new FileWriter(f));
+                // Schreibe Anzahl der Fragen, Anzahl der richtigen und Anzahl der falschen Antworten, jeweils durch Zeilenumbruch getrennt
+                writer.write(trainer.getQuestions() + System.lineSeparator() + trainer.getRight() + System.lineSeparator() + trainer.getWrong() + System.lineSeparator());
+            } finally {
+                // Schließe den BufferedWriter
+                if (writer != null) {
+                    writer.close();
+                }
+            }
+        }
+
+        // Überladene Methode, um Trainerdaten in die Standarddatei zu speichern
+        public void speichern() throws IOException{
+            speichern(this.filename);
+        }
+
+        // Methode zum Laden von Trainerdaten aus einer Textdatei
+        public void laden(String filename) throws IOException {
+            Scanner reader = new Scanner(new BufferedReader(new FileReader(filename)));
+            try {
+                // Lese Anzahl der Fragen, Anzahl der richtigen und Anzahl der falschen Antworten aus der Datei
+                int fragen = Integer.parseInt(reader.nextLine());
+                int richtige = Integer.parseInt(reader.nextLine());
+                int falsche = Integer.parseInt(reader.nextLine());
+                // Aktualisiere den Trainer mit den geladenen Werten
+                trainer.addQuestions(fragen);
+                trainer.addRight(richtige);
+                trainer.addWrong(falsche);
+            } finally {
+                // Schließe den Scanner
+                if (reader != null) {
+                    reader.close();
+                }
+            }
+        }
+
+        // Überladene Methode, um Trainerdaten aus der Standarddatei zu laden
+        public void laden() throws IOException {
+            laden(this.filename);
+        }
+
 }
