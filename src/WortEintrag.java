027dfbbf124caf6cc96aaed07d415fabe696fa54
piscessafespace <97235471+piscessafespace@users.noreply.github.com>
2024-01-19 19:59:50 +0100

    WortEintrag Basis

--- src/WortEintrag.java
+++ src/WortEintrag.java
@@ -1,2 +1,53 @@
+import javax.swing.*;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+/**
+ * Klasse für einen WortEintrag
+ * @author Aho 5bhit
+ * @version 19.01.2024
+ */
 public class WortEintrag {
+
+    private  String wort;
+    private String url;
+
+    public static boolean checkURL(String u) throws NullPointerException{
+        if(u==null) {
+            throw new NullPointerException("URL hat nichts drinnen");
+        }
+        else {
+            try {
+                URL web = new URL(u);
+                web.toURI();
+                return true;
+            } catch(MalformedURLException e) {
+                return false;
+            } catch(URISyntaxException e) {
+                return false;
+            }
+        }
+    }
+    public WortEintrag(String w, String u){
+        if(checkURL(u)==true){
+            this.url = u;
+        }
+        this.wort = w;
+    }
+
+    public void setWort(String w) {
+        try {
+            if(wort.length() >= 2) {
+                this.wort = w;
+            }
+            else {
+                throw new IllegalArgumentException("Das Wort ist zu kurz!");
+            }
+        }
+        catch(IllegalArgumentException ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage(),"ERROR", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
 }
--- src/WortListe.java
+++ src/WortListe.java
@@ -1,2 +1,9 @@
+import java.util.Arrays;
+/**
+ * Klasse für eine Wortliste Klasse
+ * @author Aho 5bhit
+ * @version 19.01.2024
+ */
 public class WortListe {
+
 }
